74e50773678148b91e29ccfd8ee801913d752caa
Paweł Wiśniewski <pawel.wis93@gmail.com>
2016-02-02 01:27:29 +0100

    First commit, old project Game of Evolution in QT - Learning basics

---
+++ mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow){
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow(){
+    delete ui;
+}
+
+void MainWindow::on_menu_quit_triggered(){
+    quick_exit(0);
+}
